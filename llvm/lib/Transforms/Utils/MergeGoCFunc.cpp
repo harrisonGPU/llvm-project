c8cc8264c556480ff36eb6a04d6c4378ee92f016
Harrison Hao <tsworld1314@gmail.com>
2024-12-22 22:24:36 +0800

    [Go] Temp.

---
+++ llvm/lib/Transforms/Utils/MergeGoCFunc.cpp
@@ -0,0 +1,114 @@
+//===-------- MergeGoCFunc.cpp - Transformations --------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/Transforms/Utils/MergeGoCFunc.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/PassManager.h"
+
+using namespace llvm;
+
+#define DEBUG_TYPE "merge-go-c-func"
+
+static cl::opt<bool>
+    RenameCallerRra("rename-caller-rra", cl::init(false),
+                    cl::desc("Rename the Go caller functions"));
+
+static cl::opt<bool>
+    RenameCalleeRra("rename-callee-rra", cl::init(false),
+                    cl::desc("Rename the Go callee functions"));
+
+static cl::opt<std::string> CallerNameRra("caller-name-rra", cl::Hidden,
+                                          cl::desc("Caller function name"),
+                                          cl::init(""));
+
+static cl::opt<std::string> CalleeNameRra("callee-name-rra", cl::Hidden,
+                                           cl::desc("Callee function name"),
+                                           cl::init(""));
+
+PreservedAnalyses MergeGoCFuncPass::run(Module &M, ModuleAnalysisManager &AM) {
+  bool Changed = false;
+  if (RenameCallerRra) {
+    if (CallerNameRra.empty()) {
+      llvm::errs()<<"RenameCaller Error: didn't specify caller function name\n";
+    }
+    renameCaller(&M);
+    Changed = true;
+  }
+  else if (RenameCalleeRra) {
+    if (CalleeNameRra.empty()) {
+        llvm::errs()<<"RenameCallee Error: didn't specify callee function name\n";
+    }
+    renameCallee(&M);
+    Changed = true;
+  }
+  return Changed ? PreservedAnalyses::none() : PreservedAnalyses::all();
+}
+
+void MergeGoCFuncPass::renameFunctionMainClosure(Module* M, std::string Suffix) {
+    std::vector<Function*> MainFuncs;
+    for (Module::iterator F = M->begin(); F != M->end(); F++) {
+        Function* Func = dyn_cast<Function>(F);
+        if (Func->isIntrinsic())  
+            continue;
+        
+        std::string FuncName = Func->getName().str();
+        FuncName = FuncName + "_" + Suffix;
+        Func->setName(FuncName);
+    }
+}
+
+void MergeGoCFuncPass::renameCaller(Module *M) {
+  std::string Suffix = CallerNameRra;
+  for (Module::iterator F = M->begin(); F != M->end(); F++) {
+    Function *Func = dyn_cast<Function>(F);
+    if (Func->isIntrinsic())  
+      continue;
+    std::string FuncName = Func->getName().str();
+    FuncName = FuncName + "_" + Suffix;
+    Func->setName(FuncName);
+  }
+}
+
+void MergeGoCFuncPass::renameCallee(Module *M) {
+  Function *MainFunc = M->getFunction("main");
+  renameRealCallee(MainFunc, "main_2nd_for_" + CalleeNameRra);
+  MainFunc->setName("main_for_" + CalleeNameRra);
+  renameFunctionMainClosure(M, CalleeNameRra);
+}
+
+void MergeGoCFuncPass::renameRealCallee(Function *MainFunc,
+                                        std::string NewCalleeName) {
+  if (!MainFunc) {
+    errs() << "renameRealCallee Error: MainFunc is null\n";
+    return;
+  }
+
+  Function *GoRtFunc = nullptr;
+  for (auto &BB : *MainFunc) {
+    for (auto &I : BB) {
+      if (auto *callInst = dyn_cast<CallInst>(&I)) {
+        if (callInst->getNumOperands() < 1) {
+          errs() << "renameRealCallee Warning: CallInst has fewer than 1 "
+                    "argument\n";
+          continue;
+        }
+        if (auto *RealMainFunc =
+                dyn_cast<Function>(callInst->getArgOperand(0))) {
+          GoRtFunc = callInst->getCalledFunction();
+          if (RealMainFunc) {
+            RealMainFunc->setName(NewCalleeName);
+            outs() << "Renamed callee function to: " << NewCalleeName << "\n";
+          }
+        } else {
+          errs() << "renameRealCallee Warning: First argument is not a "
+                    "Function*\n";
+        }
+      }
+    }
+  }
+}
